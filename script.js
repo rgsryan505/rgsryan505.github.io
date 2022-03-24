fd18130d2b8ba556250c4b9a5a1db154d7484643
Ryan <rgsryan505@gmail.com>
2022-03-24 08:12:07 -0500

    commit main

--- script.js
+++ script.js
@@ -4,3 +4,11 @@ $(document).ready(function () {
     return $(this).attr("aria-valuenow") + "%";
   });
 });
+
+$("#play").on("click", function (e) {
+  e.preventDefault();
+  $("#player")[0].src += "?autoplay=1";
+  $("#player").show();
+  $("#video-cover").hide();
+  $("#play").hide();
+});
